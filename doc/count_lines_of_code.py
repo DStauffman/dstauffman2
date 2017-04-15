862cf0eec719442ab7ff6102a48db131ac8c2cdd
David Stauffer <dstauffman@yahoo.com>
2017-04-14 17:57:40 -0700

    Add LaTeX table option and fix bug that swapped the ordering.

--- doc/count_lines_of_code.py
+++ doc/count_lines_of_code.py
@@ -12,7 +12,7 @@ Notes
 import os
 import json
 import subprocess
-from dstauffman import get_root_dir
+from dstauffman import get_root_dir, make_conclusion, make_preamble
 
 #%% Functions
 def process_repo(name, root, tests, exclude):
@@ -62,8 +62,8 @@ def process_repo(name, root, tests, exclude):
     lines['total']   = sum(data['SUM'][subkey] for subkey in types) + lines['tests']
     lines['code']    = sum(data[key]['code'] for key in langs if key in data)
     lines['comment'] = sum(data[key]['comment'] for key in langs if key in data)
-    lines['docs']    = sum(data[key][subkey] for key in docs for subkey in types if key in data)
     lines['blank']   = sum(data[key]['blank'] for key in langs if key in data)
+    lines['docs']    = sum(data[key][subkey] for key in docs for subkey in types if key in data)
     files['total']   = data['SUM']['nFiles'] + files['tests']
 
     # organize output
@@ -81,8 +81,8 @@ def print_results(name, lines, files):
     print(f" Files: {files['total']}, ({100*files['tests']/files['total']:.1f}% tests)")
     print(f" Total lines of code: {lines['total']}")
     print('  Code: {} ({:.1f}%), Comments: {} ({:.1f}%), Blank: {} ({:.1f}%), Tests: {} ({:.1f}%), Documentation: {} ({:.1f}%)'.format(\
-         lines['code'], 100*lines['code']/lines['total'], lines['tests'], 100*lines['tests']/lines['total'], \
-         lines['comment'], 100*lines['comment']/lines['total'], lines['blank'], 100*lines['blank']/lines['total'], \
+         lines['code'], 100*lines['code']/lines['total'], lines['comment'], 100*lines['comment']/lines['total'], \
+         lines['blank'], 100*lines['blank']/lines['total'], lines['tests'], 100*lines['tests']/lines['total'], \
          lines['docs'], 100*lines['docs']/lines['total']))
     print('')
 
@@ -98,6 +98,35 @@ def combine_results(out1, out2):
             for subkey in out1[key]:
                 out[key][subkey] = out1[key][subkey] + out2[key][subkey]
     return out
+
+#%% Functions - print_latex_tables
+def print_latex_tables(out, keys):
+    r"""Prints the tables for inclusion in our LaTeX documents."""
+
+    cols = ['total', 'code', 'comment', 'blank', 'tests', 'docs']
+
+    text = []
+    text += make_preamble('Count Lines of Code Breakdown','tab:cloc','lcccccc')
+    text.append(r'        \textbf{Module} & \textbf{Total} & \textbf{Code} & \textbf{Comments} & \textbf{Blank} & \textbf{Tests} & \textbf{Documentation} \\ \midrule')
+    for key in keys:
+        lines = out[key]['lines']
+        if '+' in key:
+            name = 'total'
+            midrule = ''
+        else:
+            name = key
+            midrule = r' \midrule'
+        values = ' & '.join('{}'.format(lines[x]) for x in cols)
+        text.append(r'        \multirow{2}[3]{*}{\textbf{\texttt{' + name + r'}}} & ' + values + r' \\')
+        #(100\%) & (36.0\%) & (28.6\%) & (22.4\%) & (9.3\%) & (3.7\%)
+        values = ' & '.join('({:.1f}\\%)'.format(100*lines[x]/lines['total']) for x in cols)
+        text.append(r'        & ' + values + r' \\' + midrule)
+    text += make_conclusion()
+
+    text = '\n'.join(text)
+    print(text)
+
+    return text
 
 #%% Script
 if __name__ == '__main__':
@@ -136,3 +165,7 @@ if __name__ == '__main__':
     # ghap and dstauffman
     out['ghap+dstauffman'] = combine_results(out['ghap'], out['dstauffman'])
     print_results(**out['ghap+dstauffman'])
+
+    # print the LaTeX tables
+    print_latex_tables(out, ['dstauffman', 'cromo', 'cromo+dstauffman'])
+    print_latex_tables(out, ['dstauffman', 'ghap', 'ghap+dstauffman'])
